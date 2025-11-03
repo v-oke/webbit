1889eae08c1b3fa1b0e5a5e5d1d4f4212cf9da2e
Kelvin Gicheha <kwachira@outlook.com>
2025-11-03 17:04:08 +0300

    Add create, read, update and delete for Submission

--- Gemfile
+++ Gemfile
@@ -40,7 +40,7 @@ gem "kamal", require: false
 gem "thruster", require: false
 
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
-# gem "image_processing", "~> 1.2"
+gem "image_processing", "~> 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
@@ -65,3 +65,5 @@ group :test do
 end
 
 gem "devise", "~> 4.9"
+
+gem "tailwindcss-rails", "~> 4.4"
---
+++ app/controllers/submissions_controller.rb
@@ -0,0 +1,71 @@
+class SubmissionsController < ApplicationController
+  before_action :set_submission, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, except: %i[ show index ]
+
+  # GET /submissions or /submissions.json
+  def index
+    @submissions = Submission.all
+  end
+
+  # GET /submissions/1 or /submissions/1.json
+  def show
+  end
+
+  # GET /submissions/new
+  def new
+    @submission = Submission.new
+  end
+
+  # GET /submissions/1/edit
+  def edit
+  end
+
+  # POST /submissions or /submissions.json
+  def create
+    @submission = Submission.new(submission_params)
+
+    respond_to do |format|
+      if @submission.save
+        format.html { redirect_to @submission, notice: "Submission was successfully created." }
+        format.json { render :show, status: :created, location: @submission }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /submissions/1 or /submissions/1.json
+  def update
+    respond_to do |format|
+      if @submission.update(submission_params)
+        format.html { redirect_to @submission, notice: "Submission was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @submission }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @submission.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /submissions/1 or /submissions/1.json
+  def destroy
+    @submission.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to submissions_path, notice: "Submission was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_submission
+      @submission = Submission.find(params.expect(:id))
+    end
+
+    # Only allow a list of trusted parameters through.
+    def submission_params
+      params.expect(submission: [ :title, :body, :url, :media, :user_id ])
+    end
+end
